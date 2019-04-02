f3c152ed84c47b9e2555bebd4bad6a3a63173589
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-02 10:10:03 +0900

    feat:AWSAppSyncSDKの初期化実装

--- app/src/main/java/com/example/ryosukeizumi/kotlinsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/ryosukeizumi/kotlinsampleapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ryosukeizumi.kotlinsampleapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.amazonaws.mobile.config.AWSConfiguration
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,11 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        this.
 
         button.setOnClickListener {
             val editTextInput: String = editText.text.toString()
             textView.text = editTextInput
         }
+
+        val awsAppSyncClient = AWSAppSyncClient.builder()
+            .context(applicationContext)
+            .awsConfiguration(AWSConfiguration(applicationContext))
+            .build()
+        print(awsAppSyncClient)
     }
 }
