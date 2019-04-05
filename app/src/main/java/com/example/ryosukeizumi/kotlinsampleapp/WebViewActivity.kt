04e681153e0c99a0bfe38c645b89c503810ec1df
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-05 13:53:19 +0900

    feat:WebView/Native間でメッセージのやり取りをする

--- app/src/main/java/com/example/ryosukeizumi/kotlinsampleapp/WebViewActivity.kt
+++ app/src/main/java/com/example/ryosukeizumi/kotlinsampleapp/WebViewActivity.kt
@@ -2,11 +2,15 @@ package com.example.ryosukeizumi.kotlinsampleapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JsResult
 import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_web_view.*
+import timber.log.Timber
 
 class WebViewActivity : AppCompatActivity() {
+    private val url = "http://vue-webview-app.s3-website-ap-northeast-1.amazonaws.com/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +28,25 @@ class WebViewActivity : AppCompatActivity() {
             webViewClient = WebViewClient()
         }
 
-        webView.loadUrl("http://vue-webview-app.s3-website-ap-northeast-1.amazonaws.com/")
+        // WebViewからNative側にデータを渡す
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
+                Timber.d(message)
+                result?.cancel()
+                return true
+            }
+        }
+
+        // NativeからWebViewにデータを渡す
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                val text = "{key: \"Message from Native\", value: 100}"
+                webView.evaluateJavascript("window.testFunc('$text')", null)
+            }
+        }
+
+        webView.loadUrl(url)
 //        webView.loadUrl("file:///android_asset/index.html")
+
     }
 }
